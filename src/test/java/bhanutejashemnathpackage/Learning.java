1c3fcbb3c9cf005e51227d2f27581f777f7586df
Madhushree <Madhushree@LAPTOP-R9572PD6>
2022-10-04 15:15:50 +0530

    first

---
+++ src/test/java/bhanutejashemnathpackage/Learning.java
@@ -0,0 +1,15 @@
+package bhanutejashemnathpackage;
+
+public class Learning {
+String ename;
+int eid;
+public static void main(String[] args) {
+	
+
+Learning a1=new Learning();
+System.out.println(a1.eid);
+
+	
+}
+
+}
